7919c45ab1bdff13e1c9155d9cca16cceb1863ee
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-12-17 13:01:06 -0700

    login hidden if logged in

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,116 +1,35 @@
 import React from 'react';
-import request from 'superagent';
-import LogoutButton from './LogoutButton.js';
 
 var LoginForm = React.createClass({
 
-  getInitialState() {
-    return({ username: '', password: '', message: null, token: null});
-  },
-
-  handleUsernameChange(event) {
-    this.setState({username: event.target.value});
-  },
-
-  handlePasswordChange(event) {
-    this.setState({password: event.target.value});
-  },
-
-  handleSubmit(event) {
-    event.preventDefault();
-    var username = this.state.username.trim();
-    var password = this.state.password.trim();
-
-    request
-      .post('http://localhost:8080/auth')
-      .send({
-        username,
-        password
-      })
-      .end((err, res) => {
-
-        if(!res.body.token) {
-          this.setState({
-            message: res.body.err
-          });
-        }
-
-        else {
-          this.setState({
-            message: 'Login successful!',
-            token: res.body.token
-          });
-        }
-
-        this.setState({
-          username: '',
-          password: ''
-        });
-
-      })
-  },
-
-  handleLogout(event) {
-    event.preventDefault();
-    
-    request
-      .del('http://localhost:8080/auth')
-      .send({
-        token: this.state.token
-      })
-      .end((err, res) => {
-
-        if(res.body.token) {
-          this.setState({
-            message: res.body.err
-          });
-        }
-
-        else {
-          this.setState({
-            message: 'You are no longer logged in.',
-            token: null
-          })
-        }
-
-      })
-  },
-
   render() {
 
-    var message = (this.state.message) ?
-      <p>{this.state.message}</p>
-      : null;
-
-    var logout = (this.state.token) ?
-      <LogoutButton
-        handleLogout={this.handleLogout}
-      />
+    var message = (this.props.message) ?
+      <p>{this.props.message}</p>
       : null;
 
     return (
       <div>
         <h1>Login</h1>
         {message}
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.props.handleLoginSubmit}>
           <input
             type="text"
             placeholder="username"
-            value={this.state.username}
-            onChange={this.handleUsernameChange}
+            value={this.props.username}
+            onChange={this.props.handleUsernameChange}
           />
           <input
             type="text"
             placeholder="password"
-            value={this.state.password}
-            onChange={this.handlePasswordChange}
+            value={this.props.password}
+            onChange={this.props.handlePasswordChange}
           />
           <input
             type="submit"
             value="Login"
           />
         </form>
-        {logout}
       </div>
     );
   }
--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 var LogoutButton = React.createClass({
   render() {
     return (
-      <form onSubmit={this.props.handleLogout}>
+      <form onSubmit={this.props.handleLogoutSubmit}>
         <input
           type="submit"
           value="logout"
