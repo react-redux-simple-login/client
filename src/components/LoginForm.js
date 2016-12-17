e0cf50a8c935f0b0fd64b1522acfe21a14bfbe8d
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-12-17 11:05:17 -0700

    jsx and skeleton functions complete

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,9 +5,45 @@ import request from 'superagent';
 
 var LoginForm = React.createClass({
 
+  getInitialState() {
+    return({ username: '', password: ''});
+  },
+
+  handleUsernameChange() {
+    console.log('changed username');
+  },
+
+  handlePasswordChange() {
+    console.log('changed password');
+  },
+
+  handleSubmit() {
+    console.log('handle submit');
+  },
+
   render() {
     return (
-      <h1>Hello world</h1>
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="username"
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+          />
+          <input
+            type="text"
+            placeholder="password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+          <input
+            type="submit"
+            value="Login"
+          />
+        </form>
+      </div>
     );
   }
 });
