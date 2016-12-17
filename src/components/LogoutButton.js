f631539f44c075edd9cfa312e7fdd08fe1240789
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-12-17 11:59:26 -0700

    renamed file and built skeleton component

---
+++ src/components/LogoutButton.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+import request from 'superagent';
+
+var LogoutButton = React.createClass({
+
+
+  render() {
+    return (
+
+    );
+  }
+});
+
+export default LogoutButton;
