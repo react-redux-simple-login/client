b3050fdfda987659d6c12c950e1a7e7dd39c6983
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-12-17 12:44:34 -0700

    app component added

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './LoginForm.js';
+
+var App = React.createClass({
+
+  render() {
+    return (
+      <LoginForm />
+    )
+  }
+
+});
+
+export default App;
