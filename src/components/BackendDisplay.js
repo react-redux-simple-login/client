8c9bcc3550e75c614a42514a023fd0321eb57a2f
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-12-17 15:25:13 -0700

    formatted date time with momentjs

--- src/components/BackendDisplay.js
+++ src/components/BackendDisplay.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 
 var BackendDisplay = React.createClass({
 
   render() {
 
     const { username, node_version, app_path, timestamp } = this.props.loginState;
+    const dateTime = moment(timestamp).format('MMMM Do YYYY, h:mm:ss a');
 
     return (
       <div>
         <h1>Welcome, {username}!</h1>
-        <p>{node_version}</p>
-        <p>{app_path}</p>
-        <p>{timestamp}</p>
+        <p>Node Version: {node_version}</p>
+        <p>Application path: {app_path}</p>
+        <p>Date/time: {dateTime}</p>
       </div>
     );
   }
